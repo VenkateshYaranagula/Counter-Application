55f503a3e6fe5801cfbbf75c5ba2a08808047944
Venkatesh Yaranagula <venkateshyaranagula@gmail.com>
2023-02-07 06:53:09 +0000

    Counter Application

---
+++ src/components/Counter/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+import {Component} from 'react'
+
+class Counter extends Component {
+  state = {count: 0}
+  onIncrement = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+  onDecrease = () => {
+    this.setState(prevState => ({count: prevState.count - 1}))
+  }
+  render() {
+    const {count} = this.state
+    return (
+      <div>
+        <h1>Counter Application</h1>
+        <p>{count}</p>
+        <button onClick={this.onIncrement}>Increase</button>
+        <button onClick={this.onDecrease}>Decrease</button>
+      </div>
+    )
+  }
+}
+export default Counter
